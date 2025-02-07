1fbbcd18927bc46a2cb801db91ec51232fd4948d
Mayo Horkovic <mayo.horkovic@gmail.com>
2025-02-07 17:38:21 +0700

    feat: update carousel styling and filter attribute component
    - Adjusted padding, border-radius, width, and height of `.carousel__pagination-button` in `slider.css`
    - Added `.carousel__pagination` with new positioning
    - In `FilterAttribute.vue`, replaced usage of `labels[attribute]` with a computed property `label`
    - Removed import of `labels` from `'~/utils/filter'`
    - Imported and used `useControls` to compute the label
    - Simplified formatting for author values in the serializedValue computation

--- app/components/general/FilterAttribute.vue
+++ app/components/general/FilterAttribute.vue
@@ -1,33 +1,37 @@
 <template>
   <div
-    v-if="attribute in labels"
+    v-if="label"
     class="bg-primary text-white flex items-center py-1 px-3 gap-2 rounded-3xl text-xs"
   >
     <template v-if="['has_image', 'has_iip'].includes(attribute)">
-      <span class="font-bold">{{ labels[attribute] }}</span>
+      <span class="font-bold">{{ label }}</span>
     </template>
     <template v-else>
-      <span class="font-bold">{{ labels[attribute] }}:</span>
+      <span class="font-bold">{{ label }}:</span>
       {{ serializedValue }}
     </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import { labels } from '~/utils/filter'
 import { formatAuthor } from '~/utils/formatters'
+import { useControls } from '~/composables/controls'
 
 const props = defineProps<{
   attribute: string
   value: string | string[]
 }>()
 
+const { controls } = await useControls()
+
+const label = computed(() => controls[props.attribute].label)
+
 const serializedValue = computed(() => {
   let values = props.value
   values = Array.isArray(values) ? values : [values]
 
   if (props.attribute === 'author') {
-    values = values.map((author) => formatAuthor(author))
+    values = values.map(author => formatAuthor(author))
   }
 
   return values.join(', ')
